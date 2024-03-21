b2b514d3be9c150c870b6cc80657174481cfec98
adifamiology <161519514+adifamiology@users.noreply.github.com>
2024-03-21 18:55:48 +0000

    main page text

--- ProviderDashboards.py
+++ ProviderDashboards.py
@@ -1,8 +1,12 @@
 
 import streamlit as st
 
-st.set_page_config(page_title="Client Review Dashboards", page_icon=":bar_chart:", layout="wide")
+st.set_page_config(page_title="Client Review Dashboards", page_icon=":bar_chart:", layout="wide", initial_sidebar_state="expanded")
+
+ #Header
+st.title("Welcome to Provider's View")
+st.subheader("Wealth advisor dashboards are crucial tools that enable advisors to efficiently manage client portfolios, monitor financial performance, track progress towards goals, and provide personalized advice.")
+
+st.divider()
 
 
-st.title("Main Page")
-st.sidebar.success("Select a page above")
